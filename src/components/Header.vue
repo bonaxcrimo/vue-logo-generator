4a297bb3b488a51a7d89fe8a507ff62f422f3572
admin <bonaxcrimo@gmail.com>
2019-03-10 00:09:37 +0700

    Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">Vue Logo Designer
+    <div class="socialIcons">
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/bona-tua-1b8685130/"
+            target="_blank"
+            class="socialIcons__linkedIn"
+          >
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bonaxcrimo/vue-logo-generator"
+            target="_blank"
+            class="socialIcons__github"
+          >
+            <i class="fa fa-github"></i>
+            <span>Github</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
