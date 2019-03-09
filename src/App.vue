4f505add473c6f9cb9a5a7d9d34a3a6ddbb12769
admin <bonaxcrimo@gmail.com>
2019-03-10 00:52:54 +0700

    Color Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-    <Header/>
+    <!-- <Header/> -->
+    <br>
+    <br>
+    <Color colorName="red" :options="color"></Color>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Color from "@/components/Color";
+import * as colors from "@/data/color";
 export default {
   name: "App",
+  data() {
+    return {
+      color: colors.colors.purple
+    };
+  },
   components: {
-    Header
+    Header,
+    Color
   }
 };
 </script>
---
+++ src/components/Color.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="colorOptions">
+    <ul v-if="showOptions">
+      <li
+        v-for="opt in options"
+        :key="opt"
+        v-bind:style="{ backgroundColor: opt }"
+        @click="onClickOptionHandler"
+      >{{opt}}</li>
+    </ul>
+
+    <button
+      class="button"
+      v-bind:style="{ backgroundColor: options.option2 }"
+      @click="onClickHandler"
+    >{{colorName}}</button>
+  </div>
+</template>
+<script>
+import _ from "lodash";
+export default {
+  data() {
+    return {
+      showOptions: false
+    };
+  },
+  methods: {
+    onClickHandler(e) {
+      e.preventDefault();
+      let x = !this.showOptions;
+      this.showOptions = x;
+    },
+    onClickOptionHandler(e) {
+      let selectedColor = e.target.innerHTML;
+      //   this.colorName = selectedColor;
+      this.$emit("colorName", selectedColor);
+    }
+  },
+  props: ["optionClickHandler", "colorName", "options"]
+};
+</script>
