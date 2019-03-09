2f82eee64a0e7490a64f01935f82e30cc7bd9dfa
admin <bonaxcrimo@gmail.com>
2019-03-10 01:11:47 +0700

    Component Logo

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="app">
     <Header/>
-    <div class="container"></div>
-    <ColorContainer :colors="color"></ColorContainer>
+    <div class="container">
+      <Logo :svgColorObj="svgColorObj"/>
+      <ColorContainer :colors="color"></ColorContainer>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Logo from "@/components/Logo";
 import ColorContainer from "@/components/ColorContainer";
 import * as colors from "@/data/color";
 export default {
   name: "App",
   data() {
     return {
-      color: colors.colors
+      color: colors.colors,
+      svgColorObj: {
+        strokeColor: "00D8FF",
+        strokeWidth: "24",
+        bgColor: "#222222"
+      }
     };
   },
   components: {
     Header,
-    ColorContainer
+    ColorContainer,
+    Logo
   }
 };
 </script>
